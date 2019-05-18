e4b7245ab0b1df800eec941f7cab463e8c1b0488
willianmarquesfreire <willianmarquesfreire@gmail.com>
2019-05-18 14:00:12 -0300

    Navigator object correction

--- src/simple-image.js
+++ src/simple-image.js
@@ -8,148 +8,147 @@ import takePictureModalController from './tack-picture.controller.js'
 import takePictureTemplate from './tack-picture.template.js'
 
 function simpleImage($timeout, $uibModal, $http, $rootScope, $element, $scope) {
-    var ctrl = this, inputFile;
+  var ctrl = this, inputFile;
+  if (window.navigator)
+    window.navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
+  ctrl.buttonText = ctrl.buttonText || 'Alterar Foto';
+  ctrl.buttonChangeText = ctrl.buttonChangeText || 'Carregar Foto';
+  ctrl.buttonCaptureText = ctrl.buttonCaptureText || 'Tirar Foto';
+  ctrl.buttonRemoveText = ctrl.buttonRemoveText || 'Remover Foto';
+  ctrl.typeThumbnail = ctrl.typeThumbnail || 'circle';
 
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+  var getPictureDefault = () => {
+    return ctrl.defaultPicture || 'resources/images/user-not-image.png';
+  }
 
-    ctrl.buttonText = ctrl.buttonText || 'Alterar Foto';
-    ctrl.buttonChangeText = ctrl.buttonChangeText || 'Carregar Foto';
-    ctrl.buttonCaptureText = ctrl.buttonCaptureText || 'Tirar Foto';
-    ctrl.buttonRemoveText = ctrl.buttonRemoveText || 'Remover Foto';
-    ctrl.typeThumbnail = ctrl.typeThumbnail || 'circle';
-
-    var getPictureDefault = () => {
-        return ctrl.defaultPicture || 'resources/images/user-not-image.png';
-    }
-
-    var setPictureURLByPicture = (picture, ignoreSave) => {
-      $timeout(function(){
-        if(ctrl.ngModel){
-            if (typeof ctrl.ngModel === 'string' && ctrl.isUrl(ctrl.ngModel)) {
-	            ctrl.user.pictureURL = ctrl.ngModel;
-            } else {
-	            ctrl.user.pictureURL = 'data:' + ctrl.ngModel.mimeType + ';base64,' + ctrl.ngModel.bytes;
-            }
-        }else{
-          ctrl.user.pictureURL = getPictureDefault();
+  var setPictureURLByPicture = (picture, ignoreSave) => {
+    $timeout(function () {
+      if (ctrl.ngModel) {
+        if (typeof ctrl.ngModel === 'string' && ctrl.isUrl(ctrl.ngModel)) {
+          ctrl.user.pictureURL = ctrl.ngModel;
+        } else {
+          ctrl.user.pictureURL = 'data:' + ctrl.ngModel.mimeType + ';base64,' + ctrl.ngModel.bytes;
         }
-      })
-      if(!ignoreSave)
-        savePicture();
-    }
-
-	ctrl.isUrl = url => {
-		return /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(url);
-	}
-
-    var savePicture = function(){
-      ctrl.ngModel = ctrl.user.picture;
-    }
-
-    $timeout(function(){
-      inputFile = $element.find('input')[0]
-
-      inputFile.onchange = function(event) {
-          var elm = event.srcElement || event.target;
-          if(elm.files.length == 0) return;
-          var file = elm.files[0];
-          if(file){
-            var reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = function(evt) {
-              var data = evt.target.result;
-              ctrl.openModalCropImage(data);
-            };
-          }
-      }
-      if(ctrl.user.picture){
-        setPictureURLByPicture(ctrl.user.picture, true);
-      }else{
+      } else {
         ctrl.user.pictureURL = getPictureDefault();
       }
-    }, 1000)
+    })
+    if (!ignoreSave)
+      savePicture();
+  }
 
-    ctrl.capturePicture = function() {
-      var modalInstance = $uibModal.open({
-        template: takePictureTemplate,
-        controller: takePictureModalController,
-        controllerAs: '$ctrl',
-        backdrop: 'static',
-        size: 'md',
-        resolve: {
-        }
-      });
-      modalInstance.result.then(function (picture) {
-        if(picture){
-          ctrl.openModalCropImage(picture);
-        }
-      });
+  ctrl.isUrl = url => {
+    return /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(url);
+  }
+
+  var savePicture = function () {
+    ctrl.ngModel = ctrl.user.picture;
+  }
+
+  $timeout(function () {
+    inputFile = $element.find('input')[0]
+
+    inputFile.onchange = function (event) {
+      var elm = event.srcElement || event.target;
+      if (elm.files.length == 0) return;
+      var file = elm.files[0];
+      if (file) {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function (evt) {
+          var data = evt.target.result;
+          ctrl.openModalCropImage(data);
+        };
+      }
     }
-
-    ctrl.loadPicture = function() {
-      inputFile.click()
-    };
-
-    ctrl.removePicture = function() {
-        $timeout(()=>{
-          ctrl.user.pictureURL = getPictureDefault();
-          ctrl.user.picture = null;
-          savePicture();
-        })
+    if (ctrl.user.picture) {
+      setPictureURLByPicture(ctrl.user.picture, true);
+    } else {
+      ctrl.user.pictureURL = getPictureDefault();
     }
+  }, 1000)
 
-    ctrl.openModalCropImage = function(image){
-      var modalInstance = $uibModal.open({
-        template: simpleCropTemplate,
-        controller: simpleCropController,
-        controllerAs: '$ctrl',
-        backdrop: 'static',
-        size: 'md',
-        resolve: {
-          image: function () {
-            return image;
-          },
-          type: function() {
-            return ctrl.type || 'circle';
-          }
+  ctrl.capturePicture = function () {
+    var modalInstance = $uibModal.open({
+      template: takePictureTemplate,
+      controller: takePictureModalController,
+      controllerAs: '$ctrl',
+      backdrop: 'static',
+      size: 'md',
+      resolve: {
+      }
+    });
+    modalInstance.result.then(function (picture) {
+      if (picture) {
+        ctrl.openModalCropImage(picture);
+      }
+    });
+  }
+
+  ctrl.loadPicture = function () {
+    inputFile.click()
+  };
+
+  ctrl.removePicture = function () {
+    $timeout(() => {
+      ctrl.user.pictureURL = getPictureDefault();
+      ctrl.user.picture = null;
+      savePicture();
+    })
+  }
+
+  ctrl.openModalCropImage = function (image) {
+    var modalInstance = $uibModal.open({
+      template: simpleCropTemplate,
+      controller: simpleCropController,
+      controllerAs: '$ctrl',
+      backdrop: 'static',
+      size: 'md',
+      resolve: {
+        image: function () {
+          return image;
+        },
+        type: function () {
+          return ctrl.type || 'circle';
         }
-      });
-      modalInstance.result.then(function (picture) {
-        if(picture){
-          ctrl.user.picture = picture;
-          setPictureURLByPicture(ctrl.user.picture);
-        }
-      });
+      }
+    });
+    modalInstance.result.then(function (picture) {
+      if (picture) {
+        ctrl.user.picture = picture;
+        setPictureURLByPicture(ctrl.user.picture);
+      }
+    });
+  }
+
+  $scope.$watch('$ctrl.ngModel', (data) => {
+    ctrl.user = {
+      picture: ctrl.ngModel
     }
-
-    $scope.$watch('$ctrl.ngModel', (data) => {
-        ctrl.user = {
-          picture: ctrl.ngModel
-        }
-        if(ctrl.user.picture){
-          setPictureURLByPicture(ctrl.user.picture, true);
-        }else{
-          ctrl.user.pictureURL = getPictureDefault();
-        }
-    }, true);
+    if (ctrl.user.picture) {
+      setPictureURLByPicture(ctrl.user.picture, true);
+    } else {
+      ctrl.user.pictureURL = getPictureDefault();
+    }
+  }, true);
 
 }
 
 simpleImage.$inject = ['$timeout', '$uibModal', '$http', '$rootScope', '$element', '$scope'];
 
 export default {
-    bindings: {
-      defaultPicture: '@?',
-      type: '@?',
-      ngModel: '=',
-      buttonText: '@?',
-      buttonChangeText: '@?',
-      buttonCaptureText: '@?',
-      buttonRemoveText: '@?',
-      typeThumbnail: '@?'
-    },
-    template: simpleImageTemplate,
-    controller: simpleImage
+  bindings: {
+    defaultPicture: '@?',
+    type: '@?',
+    ngModel: '=',
+    buttonText: '@?',
+    buttonChangeText: '@?',
+    buttonCaptureText: '@?',
+    buttonRemoveText: '@?',
+    typeThumbnail: '@?'
+  },
+  template: simpleImageTemplate,
+  controller: simpleImage
 }
--- src/tack-picture.controller.js
+++ src/tack-picture.controller.js
@@ -1,28 +1,29 @@
-export default function takePictureModalController($scope, $uibModalInstance, $timeout){
+export default function takePictureModalController($scope, $uibModalInstance, $timeout) {
   var video, canvas, localStream;
+  if (window.navigator) {
+    navigator.getUserMedia({ video: true }, function (stream) {
+      $timeout(function () {
+        localStream = stream;
+        video = document.getElementById("video-area-my-profile");
+        canvas = document.getElementById("canvas-area-my-profile");
+        video.srcObject = stream;
+        video.play();
+      }, 500)
+    }, function (err) {
+      if (err.name == 'PermissionDeniedError') {
+        swal('Você precisa permitir o acesso a sua Câmera.');
+        $scope.close();
+      } else if (err.name == 'DevicesNotFoundError') {
+        swal('Conecte uma câmera para tirar fotos.');
+        $scope.close();
+      } else {
+        swal('Aconteceu um erro na tentativa ligar a câmera.');
+        $scope.close();
+      }
+    });
+  }
 
-  navigator.getUserMedia({video: true}, function(stream) {
-    $timeout(function(){
-      localStream = stream;
-      video  = document.getElementById("video-area-my-profile");
-      canvas = document.getElementById("canvas-area-my-profile");
-      video.srcObject = stream;
-      video.play();
-    }, 500)
-  }, function(err) {
-    if(err.name == 'PermissionDeniedError'){
-      swal('Você precisa permitir o acesso a sua Câmera.');
-      $scope.close();
-    }else if(err.name == 'DevicesNotFoundError'){
-      swal('Conecte uma câmera para tirar fotos.');
-      $scope.close();
-    }else{
-      swal('Aconteceu um erro na tentativa ligar a câmera.');
-      $scope.close();
-    }
-  });
-
-  $scope.take = function(){
+  $scope.take = function () {
     canvas.width = video.clientWidth;
     canvas.height = video.clientHeight;
     canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -30,12 +31,12 @@ export default function takePictureModalController($scope, $uibModalInstance, $t
     $scope.close(img);
   }
 
-  $scope.close = function(img){
-    if(localStream){
-      if(localStream.stop) localStream.stop();
-      if(localStream.stopUserMedia) localStream.stopUserMedia();
+  $scope.close = function (img) {
+    if (localStream) {
+      if (localStream.stop) localStream.stop();
+      if (localStream.stopUserMedia) localStream.stopUserMedia();
       localStream.getVideoTracks().forEach(function (stream) {
-          stream.stop();
+        stream.stop();
       });
     }
     $uibModalInstance.close(img);
